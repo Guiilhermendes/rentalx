a4dccef82408bc2cd514611c66ac194842bbbd7d
Guilherme Mendes <guilhermemendeds@gmail.com>
2022-12-11 21:14:08 -0300

    Implementing S.O.L.I.D architecture in application

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,22 +1,20 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
-
-    const categoryAlredyExists = categoriesRepository.findByName(name);
-    if (categoryAlredyExists) return response.status(400).json({ error: "Category alredy exists" });
-
-    categoriesRepository.create({ name, description })
+    const createCategoryService = new CreateCategoryService(categoriesRepository);
+    createCategoryService.execute({ name, description })
     return response.status(201).send();
-})
+});
 
 categoriesRoutes.get("/", (request, response) => {
     const all = categoriesRepository.list();
     return response.status(200).json(all);
-})
+});
 
 export { categoriesRoutes };
